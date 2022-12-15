b57328477c82a77818c0953b87d2bda8219ff223
erictelkkala <eric.telkkala@gmail.com>
2022-12-15 14:42:06 +0200

    Dev server up and running
    Also:
    - Added a documentation folder

---
+++ documentation/TODO.md
@@ -0,0 +1,6 @@
+# TODO:
+
+- [ ] Come up with an idea for the project
+- [ ] Find a database (MongoDB)
+  - [ ] Write the schema to the database.md
+- [ ] Deploy the back-end
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-import express from 'express'
+import express, { Request } from 'express'
 
 const app = express()
 const port = 3001
 
-app.get('/data', (req: Request, res: any) => {
+app.use(express.json())
+
+app.get('/data', (_req: Request, res: any) => {
   res.json({ foo: 'bar' })
+})
+
+app.get('/ping', (_req: Request, res: any) => {
+  res.json({ pong: 'pong' })
 })
 
 app.listen(port, () => {
