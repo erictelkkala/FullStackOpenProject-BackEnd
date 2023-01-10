324d017acdcaf37b95823a7801d817bd700ddb31
erictelkkala <eric.telkkala@gmail.com>
2023-01-10 18:54:47 +0200

    fix(deps): updated deps

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 import mongoose from 'mongoose'
 
 import app from './app.js'
 import logger from './utils/logger.js'
-
-dotenv.config()
 
 const port = process.env.PORT || 3001
 const server_url = process.env.MONGODB_URI || ''
