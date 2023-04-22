a128b81e78e7a94e55f40cd6f668df6fbd44124c
Eric Telkkälä <52538447+erictelkkala@users.noreply.github.com>
2023-04-22 20:57:48 +0300

    refactor(userOperations): added condiitonal handling to addUser
    addUser function now logs the user added and handles errors

--- src/db/userOperations.ts
+++ src/db/userOperations.ts
@@ -4,12 +4,20 @@ import { UserModel } from './userSchema.js'
 /**
  *
  * @param user - new user with a password and a name
+ * @returns A {@link User user} document
  */
 async function addUser(user: { password: string; name: any }) {
   const newUser = new UserModel(user)
   logger.info(`Adding user ${newUser}`)
-  await newUser.save()
-  return newUser
+  await newUser
+    .save()
+    .then(() => {
+      logger.info(`User ${newUser} added`)
+      return newUser
+    })
+    .catch((e) => {
+      logger.error(e)
+    })
 }
 
 /**
@@ -18,7 +26,8 @@ async function addUser(user: { password: string; name: any }) {
  */
 async function deleteUser(id: string) {
   try {
-    UserModel.findOneAndDelete({ _id: { $eq: id } })
+    UserModel.findByIdAndRemove(id)
+    logger.info(`User with id ${id} deleted`)
   } catch (e) {
     logger.error(e)
     throw new Error('User could not be found')
